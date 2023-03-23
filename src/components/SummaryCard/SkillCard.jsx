c173ae5914a0228a4caa418df8a953b7ac278977
kethnal <kaseyvaldez@hotmail.com>
2023-03-23 02:25:13 -0700

    Import assets directly into component

--- src/components/SummaryCard/SkillCard.jsx
+++ src/components/SummaryCard/SkillCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import memory from "../../../public/assets/memory.svg";
+import reaction from "../../../public/assets/reaction.svg";
+import verbal from "../../../public/assets/verbal.svg";
+import visual from "../../../public/assets/visual.svg";
 
 function SkillCard(props) {
+
+  function getIcon() {
+    if (props.datum.category === "Memory") return memory;
+    if (props.datum.category === "Reaction") return reaction;
+    if (props.datum.category === "Verbal") return verbal;
+    if (props.datum.category === "Visual") return visual;
+  }
+
+  const icon = getIcon();
+
   return (
     <div className={`skillcard skillcard-${props.datum.category}`}>
 
       <div className='skillcard-category'> 
-        <img src={`/public/assets/icon-${props.datum.category.toLowerCase()}.svg`} alt={`${props.datum.category} icon`} />
+        <img src={icon} alt={`${props.datum.category} icon`} />
         <p>{props.datum.category}</p>
       </div>
 
