e8583f0b16177a849de564b4e6d1ae44cb441146
kethnal <kaseyvaldez@hotmail.com>
2023-03-23 01:38:50 -0700

    Move assets to public

--- src/data.jsx
+++ src/data.jsx
@@ -2,21 +2,21 @@ export const data = [
   {
     "category": "Reaction",
     "score": 80,
-    "icon": "src/assets/icon-reaction.svg"
+    "icon": "public/assets/icon-reaction.svg"
   },
   {
     "category": "Memory",
     "score": 92,
-    "icon": "src/assets/icon-memory.svg"
+    "icon": "public/assets/icon-memory.svg"
   },
   {
     "category": "Verbal",
     "score": 61,
-    "icon": "src/assets/icon-verbal.svg"
+    "icon": "public/assets/icon-verbal.svg"
   },
   {
     "category": "Visual",
     "score": 72,
-    "icon": "src/assets/icon-visual.svg"
+    "icon": "public/assets/icon-visual.svg"
   }
 ];
